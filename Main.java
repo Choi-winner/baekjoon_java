a7b94d61ce5cdad638e06761cf055b0184e4524b
Choi-winner <75618695+Choi-winner@users.noreply.github.com>
2020-12-20 16:35:58 +0900

    [baekjoon-10809] the position of all alphabets.
    [baekjoon-10809]Searching the position of all alphabets.

--- Main.java
+++ Main.java
@@ -1,49 +1,38 @@
-package baekjoon.P11720;
+package baekjoon.P10809;
+// Searching the position of all alphabets.
 
 import java.util.Scanner;
 
+
 public class Main {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		int N1 = sc.nextInt();
+		String S = sc.nextLine();
 
-		
-		String N2 = sc2.nextLine();
-		String N3 = N2;	
-		
-		int sum = 0;
-		if(N1 > 9) {
-				String temp_str = N2.substring(0,9);
-				long temp = Long.parseLong(temp_str);
-				for(int i = 0; i < 9 ; i++) {	
-					sum += temp%10;
-					temp /= 10;
-					if(temp == 0)
-						break;
-				}
-				String temp_str2 = N3.substring(9,N2.length());
-				long temp2 = Long.parseLong(temp_str2);          
-				for(int i = 0; i < N2.length()-9 ; i++) {
-					sum += temp2%10;
-					temp2 /= 10;
-					if(temp2 == 0)
-						break;
-				}
-		}
-		else {
-			String str3 = N2;
-			long temp3 = Long.parseLong(str3);
-			for(int i = 0; i < 15 ; i++) {
-				sum += temp3%10;
-				temp3 /= 10;
-				if(temp3 == 0)
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 97+0; i < 97+26; i++ ) {
+			char ch = (char)i;
+
+			for(int j = 0; j < S.length() ; j++) {
+				if(S.charAt(j) == ch) {
+					sb.append(Character.forDigit(j, 10));
 					break;
-			}			
+				}
+				if(j == S.length()-2) {
+					int p = 1;
+					sb.append('-');
+					sb.append(Character.forDigit(p, 10));
+					break;
+				}
+			}
+			if(i == 97+26-1)
+				break;
+			sb.append(' ');
 		}
-		System.out.println(sum);
+		String str = sb.toString();
+		System.out.println(str);
+	
 	}
 
 }
