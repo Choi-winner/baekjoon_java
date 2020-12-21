a6dd2d7b79a9a6cae8c3d23675a8c361d67967fd
Choi-winner <75618695+Choi-winner@users.noreply.github.com>
2020-12-21 15:21:06 +0900

    BufferedWriter를 사용하여 출력하는 방식.
    속도가 156ms에서 132ms로 빨라짐!
    앞으로 System.out.println 말고 BufferedWriter 불러와서 write 메소드로 출력하는 편이 나을듯.

--- baekjoon_P1193.java
+++ baekjoon_P1193.java
@@ -3,29 +3,36 @@ package baekjoon;
 //Finding the fraction in the 2by2 array.
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class baekjoon_P1193 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		String s = bf.readLine();
-		int N = Integer.parseInt(s);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(bf.readLine());
 		bf.close();
+		StringBuffer sb = new StringBuffer(); //StringBuffer에 String 저장.
 		
 		int layer = 1;
 		while( N > 0 ) {
 			if(N <= layer) {
 				if(layer%2 == 0)  // 짝수 층
-					System.out.println(N+"/"+(layer+1-N));
+					sb.append(N).append("/").append(layer+1-N); // 저장하는 방식.
 				else  // 홀수 층
-					System.out.println((layer+1-N)+"/"+N);			
+					sb.append(layer+1-N).append("/").append(N);	
 				break;
 			} else { // N > layer
 				N -= layer;
 				layer++; // go to next layer.	
 			}
 		}
+		bw.write(sb.toString()); // BufferedWriter에 의해 출력하는 방법.
+		bw.flush();
+		bw.close();
 	}
 }
